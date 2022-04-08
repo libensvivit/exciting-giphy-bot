f534e4f20c6d828fab0fc14508615511bcc07d3b
Achmet Tachsin <41206101+libensvivit@users.noreply.github.com>
2022-04-08 16:42:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,27 @@
-"""
-Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
-Deployed using heroku.
-Author: liuhh02 https://medium.com/@liuhh02
-"""
+import os, telebot
+import requests, sys, random
+TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
+GIPHY_API_KEY = os.environ['GIPHY_API_KEY']
 
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', 5000))
+bot = telebot.TeleBot(TELEGRAM_API_KEY)
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+def getGiphyImage(search_keyword):
+  URL = 'http://api.giphy.com'
+  SEARCH_PATH = '/v1/gifs/search'
+  payload = {'api_key': GIPHY_API_KEY, 'q': search_keyword}
+  r = requests.get(URL + SEARCH_PATH, params=payload)
+  data = r.json()['data']
+  image_position = random.randrange(0, len(data))
+  image_url = data[image_position]['images']['original']['url']
+  return image_url
 
-logger = logging.getLogger(__name__)
-TOKEN = '5225421589:AAEq4Hr37vkVO4_2YYVTwHSTc6QxhlkD0aU'
+@bot.message_handler(commands=['greet'])
+def greet(msg):
+  search = msg.text.split()[1]
+  res = getGiphyImage(search)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+  bot.send_animation(msg.chat.id, res, caption="EVEDO TO 1$ !!")
+  
+  #bot.reply_to(msg, res)
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.set_webhook(url="https://exciting-giphy-bot.herokuapp.com/", certificate=open('mycert.pem'))
