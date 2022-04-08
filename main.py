b120b6776394a029200ba8bdb61978ea498ce71e
Achmet Tachsin <41206101+libensvivit@users.noreply.github.com>
2022-04-08 19:31:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,28 +9,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-def getYoutube(search_keyword):
-    return 0
-
-def getPornhub(search_keyword):
-    URL = 'https://www.pornhub.com/video/search?search='
-    response = requests.get(URL + search_keyword)
-    soup = BeautifulSoup(response.text, 'lxml')
-    ph_links = []
-    for a in soup.find_all('a', href=True):
-        if a['href'].startswith("/view_video"):
-            ph_links.append('https://www.pornhub.com' + a['href'])
-    return random.choice(ph_links)
-
-def getGiphyImage(search_keyword):
-  URL = 'http://api.giphy.com'
-  SEARCH_PATH = '/v1/gifs/search'
-  payload = {'api_key': 'gKs8c63gf2e6Vq75MlQBWpuW1yCD7TsC', 'q': search_keyword}
-  r = requests.get(URL + SEARCH_PATH, params=payload)
-  data = r.json()['data']
-  image_position = random.randrange(0, len(data))
-  image_url = data[image_position]['images']['original']['url']
-  return image_url
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def help(update, context):
     update.message.reply_text(
@@ -38,19 +18,32 @@ def help(update, context):
         '/pornhub <search>'
         )
 
-def youtube(update, context):
-    update.message.reply_text(getYoutube(update.message.text.split()[1]))
+def quote(update, context):
+    response = requests.get("https://api.quotable.io/random")
+    update.message.reply_text(response.content+"\n-"+response.author)
 
 def pornhub(update, context):
-    update.message.reply_text(getPornhub(update.message.text.split()[1]))
+    search_keyword = update.message.text.split()[1]
+
+    URL = 'https://www.pornhub.com/video/search?search='
+    response = requests.get(URL + search_keyword)
+    soup = BeautifulSoup(response.text, 'lxml')
+    ph_links = []
+    for a in soup.find_all('a', href=True):
+        if a['href'].startswith("/view_video"):
+            ph_links.append('https://www.pornhub.com' + a['href'])
+    update.message.reply_text(random.choice(ph_links))
 
 def giphy(update, context):
-    search = "-".join(update.message.text.split()[1:])
-    update.message.reply_text(search)
-    update.message.reply_animation(getGiphyImage(search))
-
-def error(update, context):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    search_keyword = "-".join(update.message.text.split()[1:])
+    URL = 'http://api.giphy.com'
+    SEARCH_PATH = '/v1/gifs/search'
+    payload = {'api_key': 'gKs8c63gf2e6Vq75MlQBWpuW1yCD7TsC', 'q': search_keyword}
+    r = requests.get(URL + SEARCH_PATH, params=payload)
+    data = r.json()['data']
+    image_position = random.randrange(0, len(data))
+    image_url = data[image_position]['images']['original']['url']
+    update.message.reply_animation(image_url)
 
 
 def main():
@@ -60,7 +53,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("giphy", giphy))
     dp.add_handler(CommandHandler("pornhub", pornhub))
-    dp.add_handler(CommandHandler("youtube", youtube))
+    dp.add_handler(CommandHandler("quote", quote))
 
     dp.add_error_handler(error)
 
