8479cbe67f2746d7053a251e661356696a8ea5b8
Achmet Tachsin <41206101+libensvivit@users.noreply.github.com>
2022-04-08 18:51:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def getGiphyImage(search_keyword):
 def giphy(update, context):
     search = '-'.join(update.message.text.split()[1:])
     update.message.reply_text(search)
-    update.message.reply_animation(getGiphyImage(search))
+    #update.message.reply_animation(getGiphyImage(search))
 
 def help(update, context):
     update.message.reply_text('/giphy <search> \n/pornhub <search>')
