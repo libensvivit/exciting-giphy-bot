8b8ba4acfbc4f5ea650a554a2e6722de5d394f82
Achmet Tachsin <41206101+libensvivit@users.noreply.github.com>
2022-04-08 17:20:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,10 @@ def help(update, context):
     update.message.reply_text('Currently I am in Alpha stage, help me also!')
 
 def greet(update, context):
-    giphy = getGiphyImage(update.message.text)
-    #update.message.reply_text(context)
-    update.message.reply_animation(giphy)
+    #giphy = getGiphyImage(update.message.text[1])
+    update.message.reply_text(update.message.text)
+    update.message.reply_text(update.message.text[1])
+    #update.message.reply_animation(giphy)
 
 
 def error(update, context):
