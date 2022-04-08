17d8433b880867453bed8fcbc833c2426abd5419
Achmet Tachsin <41206101+libensvivit@users.noreply.github.com>
2022-04-08 16:46:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,68 @@
-import os, telebot
-import requests, sys, random
-TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
-GIPHY_API_KEY = os.environ['GIPHY_API_KEY']
+import logging
 
-bot = telebot.TeleBot(TELEGRAM_API_KEY)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-def getGiphyImage(search_keyword):
-  URL = 'http://api.giphy.com'
-  SEARCH_PATH = '/v1/gifs/search'
-  payload = {'api_key': GIPHY_API_KEY, 'q': search_keyword}
-  r = requests.get(URL + SEARCH_PATH, params=payload)
-  data = r.json()['data']
-  image_position = random.randrange(0, len(data))
-  image_url = data[image_position]['images']['original']['url']
-  return image_url
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-@bot.message_handler(commands=['greet'])
-def greet(msg):
-  search = msg.text.split()[1]
-  res = getGiphyImage(search)
+logger = logging.getLogger(__name__)
 
-  bot.send_animation(msg.chat.id, res, caption="EVEDO TO 1$ !!")
-  
-  #bot.reply_to(msg, res)
 
-bot.set_webhook(url="https://exciting-giphy-bot.herokuapp.com/", certificate=open('mycert.pem'))
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hey this is your bot!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Currently I am in Alpha stage, help me also!')
+
+def piracy(update, context):
+    update.message.reply_text('Ahhan, FBI wants to know your location!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("5225421589:AAEq4Hr37vkVO4_2YYVTwHSTc6QxhlkD0aU", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("piracy", piracy))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
